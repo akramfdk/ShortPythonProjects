807984cdcba21fe3abd40f3bd1d2dd3b210381e1
akramfdk <akramjeet.singh@gmail.com>
2023-09-06 13:38:08 +1000

    Improvement to Hangman
    Separate package created for word list and ascii art

---
+++ HangmanFiles/HangmanWordList.py
@@ -0,0 +1,12 @@
+word_list = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon",
+    "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "watermelon",
+    "apricot", "blueberry", "cranberry", "dragonfruit", "guava", "kiwifruit", "lime", "mulberry", "pineapple", "raspberry",
+    "blackberry", "boysenberry", "currant", "eggplant", "grapefruit", "leek", "onion", "pepper", "squash", "tomato",
+    "avocado", "broccoli", "cucumber", "lettuce", "parsnip", "spinach", "turnip", "zucchini", "bean", "carrot",
+    "pea", "potato", "sweetcorn", "yam", "beetroot", "celery", "garlic", "mushroom", "pumpkin", "swede",
+    "artichoke", "cauliflower", "greenbean", "olive", "radish", "sweetpotato", "bellpepper", "corn", "kale", "pepper",
+    "peppermint", "rosemary", "thyme", "basil", "cinnamon", "ginger", "oregano", "sage", "vanilla", "cayenne",
+    "coriander", "parsley", "salt", "blackpepper", "paprika", "turmeric", "chocolate", "caramel", "vanilla", "butterscotch",
+    "strawberry", "blueberry", "mint", "coconut", "coffee", "espresso", "cappuccino", "latte", "macchiato", "mocha"
+]
